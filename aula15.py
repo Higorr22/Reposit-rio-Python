ef26b07881588f241835cef0340cda3357e3e688
Higorr22 <105500337+Higorr22@users.noreply.github.com>
2025-06-11 16:55:57 -0300

    aula15.py
    Aula referencia a utilização da função 'input' no Python.

---
+++ aula15.py
@@ -0,0 +1,9 @@
+numero_1 = input('Digite uma número:')
+numero_2 = input('Digite outro número:')
+
+#Correção do tipo STR
+
+int_numero_1  = int(numero_1)
+int_numero_2 =  int(numero_2)
+
+print(f' A soma dos números é: {int_numero_1 + int_numero_2})
