783108b52d394d93e1fdd9c5e63babef6296789f
Higorr22 <105500337+Higorr22@users.noreply.github.com>
2025-07-01 13:17:22 -0300

    Aula3.py
    Aprendizagem sobre Tipagem de Programação, Tipos de dados

---
+++ Aula3.py
@@ -0,0 +1,16 @@
+"""
+Docstring é um tipo de String, pois está dentro de aspas
+Python = É uma linguagem de programação
+Tipagem de programação = Dinâmica/Forte (O Python consegue interpretar o caractér utilizado no desnvolvimento. )
+str / string = um texto utilizado dentro de aspas simples ou duplas
+Strings então, são textos que estão dentro de aspas
+"""
+print(1234)
+# Aspas Simples
+print('Higor Rodrigues')
+# Aspas Duplas
+print("Higor Rodrigues")
+# Escape (Fala para o interpretador, que ao ver o caracter de escape ele pule a sentença)
+print('Higor \'Rodrigues')
+# r (Tem como objetivo marcar o caracter de escape.)
+print(r"Higor \'Rodrigues\'")
