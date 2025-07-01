76c33cd2abaa80d92b90d6e30049f89cc1411b6d
Higorr22 <105500337+Higorr22@users.noreply.github.com>
2025-07-01 13:06:11 -0300

    Aula1.py
    Compreensão de Docstring e Função Print

---
+++ Aula1.py
@@ -0,0 +1,17 @@
+"""
+E escrever o que eu quiser
+Docstring
+
+Entendendo o que é Docstring e introdução da função print no Python
+
+"""
+
+
+'''
+Usar para escrever notas
+'''
+
+# Permite escrever um comentário
+print(123), #Na frente
+# Abaixo
+print(456)
